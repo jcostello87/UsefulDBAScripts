6944e4856bb233ddabe39e38f3d85a69034ec160
jcostello87 <52507114+jcostello87@users.noreply.github.com>
2025-11-10 11:06:13 -0500

    Delete UploadtoContainer.ps1

--- UploadtoContainer.ps1
+++
@@ -1,9 +0,0 @@
-﻿$StorageAccountName = "storageaccountnamevalue"
-$StorageAccountKey = "storageaccountkeyvalue"
-$ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
-$ContainerName = "sql02-20190627"
-New-AzureStorageContainer -Name $ContainerName -Context $ctx -Permission Blob
-$localFileDirectory = "F:\"
-$BlobName= "SQL02"
-$localfile = $localFileDirectory + $BlobName
-ls -File $localFile -Recurse | Set-AzureStorageBlobContent -Container $ContainerName -Context $ctx
