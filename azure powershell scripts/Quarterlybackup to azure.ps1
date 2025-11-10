52d91c157d3c35a32a2528475c80d46897b1f649
jcostello87 <52507114+jcostello87@users.noreply.github.com>
2025-11-10 11:09:52 -0500

    Add files via upload

---
+++ azure powershell scripts/Quarterlybackup to azure.ps1
@@ -0,0 +1,11 @@
+﻿#All files in folders
+$StorageAccountName = "storageaccountnamevalue"
+$StorageAccountKey = "storageaccountkeyvalue"
+$ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+$Today = get-date -format "yyyyMMdd"
+$ContainerName = "sql-01-" + $Today
+New-AzureStorageContainer -Name $ContainerName -Context $ctx -Permission Blob
+$localFileDirectory = "E:\"
+$BlobName= "Staging"
+$localfile = $localFileDirectory + $BlobName
+ls -File $localFile -Recurse | Set-AzureStorageBlobContent -Container $ContainerName -Context $ctx
