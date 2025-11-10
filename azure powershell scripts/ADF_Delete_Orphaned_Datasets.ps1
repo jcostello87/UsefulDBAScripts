1d182bfec6e5287db4f5a54105e014215b8cc4e5
jcostello87 <52507114+jcostello87@users.noreply.github.com>
2025-11-10 11:22:19 -0500

    Add files via upload

---
+++ azure powershell scripts/ADF_Delete_Orphaned_Datasets.ps1
@@ -0,0 +1,5 @@
+﻿Connect-AzureRmAccount
+
+Select-AzureRmSubscription FEI-AZ-PRODUCTION-1
+
+Get-AzureRmDataFactoryV2 -ResourceGroupName feipeu2-dataservices-azuredatafactory-rg-001 -Name FEI-ADF-DCMGMT  | Get-AzureRmDataFactoryV2Dataset |Remove-AzureRmDataFactoryV2Dataset -Force
