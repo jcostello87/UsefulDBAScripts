5873863d3268615392002ed1950bca8523fd752d
jcostello87 <52507114+jcostello87@users.noreply.github.com>
2026-04-02 18:08:08 -0500

    Update and rename searchstoredprocedures.sql to searchdbobjects.sql

--- SQL Scripts/searchdbobjects.sql
+++ SQL Scripts/searchdbobjects.sql
@@ -23,3 +23,23 @@ OR sc.TEXT LIKE '%tvCurrentGPH%'
 OR sc.TEXT LIKE '%tblFuture_Forecast_Details%'
 
 AND TYPE = 'P' 
+
+--look for columns
+
+select * from INFORMATION_SCHEMA.columns where column_name = 'Whse'
+
+ 
+
+--look for indexes
+
+select top 100
+
+i.name as indexName,c.name as columnName, i.type_desc
+
+from sys.indexes i
+
+inner join sys.index_columns ic on i.object_id= ic.object_id and i.index_id=ic.index_id
+
+inner join sys.columns c on ic.object_id=c.object_id ANd ic.column_id = c.column_id
+
+WHERE c.name like '%request_id%'
